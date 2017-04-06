27d7a68532b7d6bf42e5c829a29c97ba81fe02a1
luisfediaz <pipexir@gmail.com>
2017-04-06 01:52:31 -0500

    starting course

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by luisfediaz on 5/04/17.
+ */
+
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function(){
+    return <div> hi </div>;
+};
+
+ReactDOM.render(App);
